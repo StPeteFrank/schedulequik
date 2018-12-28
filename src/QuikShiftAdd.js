f56f0511d1ea450aa0489e0739023e19795731ad
Frank Diaz <diazfrankp@gmail.com>
2018-12-28 15:47:39 -0500

    Added days radio inputs to QuikShiftAdd.

--- src/QuikShiftAdd.js
+++ src/QuikShiftAdd.js
@@ -19,6 +19,30 @@ class QuikShiftAdd extends Component {
               <input type="text" placeholder="Out Time" />
             </div>
 
+            <div className="ApplyToDaysInput">
+              <p>Apply to Days</p>
+              <div className="QuikShiftAddDaysInput">
+                <input type="radio" /> <label>Monday</label>
+              </div>
+              <div className="ListedPositions">
+                <input type="radio" /> <label>Tuesday</label>
+              </div>
+              <div className="ListedPositions">
+                <input type="radio" /> <label>Wednesday</label>
+              </div>
+              <div className="ListedPositions">
+                <input type="radio" /> <label>Thursday</label>
+              </div>
+              <div className="ListedPositions">
+                <input type="radio" /> <label>Friday</label>
+              </div>
+              <div className="ListedPositions">
+                <input type="radio" /> <label>Saturday</label>
+              </div>
+              <div className="ListedPositions">
+                <input type="radio" /> <label>Sunday</label>
+              </div>
+            </div>
             <div className="QuikShiftAddButtons">
               <button>Add Shift</button>
               <button>Cancel</button>
