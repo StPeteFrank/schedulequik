546994fd689c748ed93eda3d4704f5d6b8ea03f4
Frank Diaz <diazfrankp@gmail.com>
2018-12-28 12:05:23 -0500

    Added comments to AddPositions and EditPositions.

--- src/AddPositions.js
+++ src/AddPositions.js
@@ -12,17 +12,21 @@ class AddPositions extends Component {
             </div>
             <div>
               <input type="text" placeholder="Enter Position" />
+              {/* User inputs text of new postion and clicks Add button to post onto db (HttpPost). */}
+              {/* Then newly put position appears under PositionsList section (httpGet). */}
             </div>
           </section>
 
           <section className="PositionsList">
             <div className="EditDeleteButton">
               <p>Positions</p> <button>Edit/Delete</button>
+              {/* Edit/Delete button takes user to EditPositions view. */}
             </div>
             <li>Office Manager</li>
             <li>Developer</li>
             <li>Senior Developer</li>
             <li>CTO</li>
+            {/* These are hard-coded and will need to get positions from db (HttpGet). */}
           </section>
         </div>
       </div>
