ed95f89b8eb5c466924062cf0e4e3076e562ff9f
Frank Diaz <diazfrankp@gmail.com>
2019-02-11 10:19:09 -0500

    Github link at footer.

--- src/Footer.js
+++ src/Footer.js
@@ -5,10 +5,18 @@ class Footer extends Component {
     return (
       <div className="Footer">
         <h9>ScheduleQuik</h9>
-        <h9> &copy;2019 github.com/StPeteFrank</h9>
+        <a
+          className="Links"
+          href="https://www.github.com/StPeteFrank"
+          target="_blank"
+        >
+          &copy;2019 github.com/StPeteFrank
+        </a>
       </div>
     )
   }
 }
-
+{
+  /* <h9> &copy;2019 github.com/StPeteFrank</h9> */
+}
 export default Footer
